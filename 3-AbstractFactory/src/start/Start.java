d5aea6c2c9c0820c3bf547dd5c307e231188971e
liza <liza@DESKTOP-M4PO6CG>
2020-07-03 16:56:02 +0300

    AbstractFactory

---
+++ 3-AbstractFactory/src/start/Start.java
@@ -0,0 +1,26 @@
+package start;
+
+import factory.impl.RussianFactory;
+import factory.impl.USAFactory;
+import factory.interfaces.TransportFactory;
+
+public class Start {
+    
+    private static TransportFactory factory;
+    
+    public static void main(String[] args) {
+      
+        if (true){
+            factory = new RussianFactory();
+        }else{
+            factory = new USAFactory();
+        }
+        
+        
+        factory.createAircraft();
+        factory.createCar();
+        
+        
+    }
+
+}
